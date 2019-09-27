a5a797ff3061c0b6fb53f6a0049fa4d69ce02c63
Eric Lee <Raidenlkl@gmail.com>
2019-09-27 23:18:41 +1000

    modularize

---
+++ data_visualization.R
@@ -0,0 +1,122 @@
+# numeric_cols_names <- select(uniData,2:15) %>%
+#   select_if(is.numeric) %>% 
+#   colnames()
+
+# using an appropriate graph
+visualize_categorical_data <- function(uniData){
+  
+  # Global Variable
+  category_cols_names <- uniData %>%
+    select_if(is.factor) %>% 
+    colnames()
+  
+  # Fill = The color of the area
+  # Color = The border color
+  for (colname in category_cols_names ){
+    barChart = ggplot(uniData, aes(x = uniData[,colname], fill= uniData[,colname] )) + 
+      geom_bar() + 
+      labs( x = colname, 
+            y = "Number of Students", 
+            fill = colname,
+            title = paste(colname,"Bar Chart"))
+    
+    print(barChart)
+  }
+}
+
+# using an appropriate graph
+visualize_numerical_data <- function(uniData){
+  
+  # Density Graph
+  print ( 
+    ggplot(uniData, aes(x = Age )) + 
+      geom_density() + 
+      labs(x = "Age", y = "Frequency", title = "Density Graph of Age") + 
+      xlim(18,35) 
+  )
+  
+  print ( 
+    ggplot(uniData, aes(x = Achieved_Credit_Points )) + 
+      geom_density() + 
+      xlim(0, 220) + 
+      labs(x = "Achieved_Credit_Points" , y = "Frequency", title = "Density Graph of Achieved_Credit_Points" ) 
+  )
+  
+  # Histogram
+  print (
+    ggplot(uniData, aes(x = GPA )) + 
+      geom_histogram(bins = 14, color="darkblue", fill="lightblue") + 
+      labs( x = "GPA" , y = "Number of Students", title = "Histogram of GPA") 
+  )
+  
+  print (
+    ggplot(uniData, aes(x = OP_Score )) + 
+      geom_histogram(bins = 10, color="darkblue", fill="lightblue") + 
+      labs( x = "OP_Score" , y = "Number of Students", title = "Histogram of OP Score" ) 
+  )
+  
+  # Without Log
+  print (
+    ggplot(uniData, aes(x = Failed_Credit_Points )) + 
+      geom_histogram(bins = 5, color="darkblue", fill="lightblue") + 
+      labs( x = "Failed_Credit_Point" , y = "Number of Students", title = "Histogram of Failed_Credit_Point" ) 
+  )
+  
+}
+
+visualize_boxplot_gpa_vs_gender <- function(uniData){
+  
+  # Visualize boxplot graph (GPA Vs Gender)
+  print(
+    uniData %>%
+      ggplot(aes(x= Gender, y = GPA)) + 
+      geom_boxplot() + 
+      labs( x= "Gender", title = "BoxPlot ( GPA vs Gender )" )  
+  )
+  
+  # T Test
+  t.test(uniData$GPA ~ uniData$Gender)
+  # Variance
+  var.test(uniData$GPA ~ uniData$Gender)
+  
+}
+
+visualize_relationship_op_and_gpa <- function(uniData){
+  print(
+    uniData %>%
+      mutate(GPA_Round = as.factor(round(GPA)) ) %>%
+      ggplot( aes(x = OP_Score, fill = GPA_Round) ) + 
+      geom_bar(position = "fill") + 
+      labs( x = "OP Score ( 1 (highest) -> 25 (lowest) performance )", 
+            y = "proportion of students getting Each OP score", 
+            title = "OP Score Barchart (colored by Each GPA Group) ") 
+  )
+  
+  print(
+    uniData %>%
+      mutate(GPA_Round = as.factor(round(GPA)) ) %>%
+      ggplot( aes(x = OP_Score) ) + 
+      geom_histogram(bins = 10, color="darkblue", fill="lightblue") + 
+      facet_wrap(~GPA_Round) +
+      labs( x = "OP Score ( 1 (highest) -> 25 (lowest) performance )", 
+            y = "Number of Students", 
+            title = "OP Score histogram (Divided by Each GPA Group) ")  
+  )
+}
+
+visualize_scatterplots_Vs_GPA <-function(uniData){
+  
+  scatter.smooth(x=uniData$Age, y=uniData$GPA, main="Age vs GPA")
+  cor(uniData$Age, uniData$GPA)
+  
+  scatter.smooth(x= uniData$OP_Score, y= uniData$GPA, main="OP_Score vs GPA")
+  cor(uniData$OP_Score, uniData$GPA)
+  
+  scatter.smooth(x= uniData$Achieved_Credit_Points, y= uniData$GPA, main="Achieved_Credit_Points vs OP_Score")
+  cor(uniData$Achieved_Credit_Points, uniData$GPA)
+  
+  scatter.smooth(x= uniData$Failed_Credit_Points, y= uniData$GPA, main="Failed_Credit_Points vs OP_Score")
+  cor(uniData$Failed_Credit_Points, uniData$GPA)
+  
+}
+
--- portfolio2.R
+++ portfolio2.R
@@ -2,112 +2,44 @@
 library(ggplot2)
 library(dplyr)
 
-# Import Data 
-uniData <- read.csv(file.choose(), header = TRUE)
+# Setting up the working directory
+# So that It can import external file
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+source("data_visualization.R")
 
-# Global Variable
-category_cols_names <- select(uniData,2:15) %>%
-  select_if(is.factor) %>% 
-  colnames()
+# Import Data 
+uniData <- read.csv("datasets/Portfolio_2_data.csv", header = TRUE) %>%
+  select(2:15)
 
 # Task 1  ----------------------------------------------------------------------
 # Summary the information in each variable (expect case ID) using a table
-select(uniData,2:15) %>%
+uniData %>%
   summary()
 
-# using an appropriate graph
-displayBarChart <- function(){
-  # Fill = The color of the area
-  # Color = The border color
-  for (colname in category_cols_names ){
-    barChart = ggplot(uniData, aes(x = uniData[,colname], fill= uniData[,colname] )) + 
-        geom_bar() + 
-        labs( x = colname, 
-              y = "Number of Students", 
-              fill = colname,
-              title = paste(colname,"Bar Chart"))
-    
-    print(barChart)
-  }
-}
-
-# using an appropriate graph
-displayNumericGraphs <- function(){
-  
-  # Density Graph
-  print ( 
-    ggplot(uniData, aes(x = Age )) + 
-    geom_density() + 
-    labs(x = "Age", y = "Frequency", title = "Density Graph of Age") + 
-    xlim(18,35) 
-  )
-  
-  print ( 
-    ggplot(uniData, aes(x = Achieved_Credit_Points )) + 
-    geom_density() + 
-    xlim(0, 220) + 
-    labs(x = "Achieved_Credit_Points" , y = "Frequency", title = "Density Graph of Achieved_Credit_Points" ) 
-  )
-  
-  # Histogram
-  print (
-    ggplot(uniData, aes(x = GPA )) + 
-    geom_histogram(bins = 14, color="darkblue", fill="lightblue") + 
-    labs( x = "GPA" , y = "Number of Students", title = "Histogram of GPA") 
-  )
-  
-  print (
-    ggplot(uniData, aes(x = OP_Score )) + 
-    geom_histogram(bins = 10, color="darkblue", fill="lightblue") + 
-    labs( x = "OP_Score" , y = "Number of Students", title = "Histogram of OP Score" ) 
-  )
-  
-  # Without Log
-  print (
-    ggplot(uniData, aes(x = Failed_Credit_Points )) + 
-    geom_histogram(bins = 5, color="darkblue", fill="lightblue") + 
-    labs( x = "Failed_Credit_Point" , y = "Number of Students", title = "Histogram of Failed_Credit_Point" ) 
-  )
-  
-}
-
 # Appropriate way to display categorical Data
-displayBarChart()
+visualize_categorical_data(uniData)
 # Appropriate way to display numeric Data
-displayNumericGraphs()
+visualize_numerical_data(uniData)
 
 # Task 2 ----------------------------------------------------------
 # Compare average GPA between Male and Female 
 # Conduct a statistical Test
 # Interpret its results
-attach(uniData)
-
-uniData %>%
-  ggplot(aes(x=Gender, y = GPA)) + geom_boxplot() + labs( x = "BoxPlot ( GPA vs Gender )" ) 
-
-# T Test
-t.test(GPA ~ Gender)
-# Variance
-var.test(GPA ~ Gender)
+visualize_boxplot_gpa_vs_gender(uniData)
 
 # Task 3 ----------------------------------------------------------
 # Explore the relationship between OP Score and GPA using Graph 
-
-uniData %>%
-  mutate(GPA_Round = as.factor(round(GPA)) ) %>%
-  ggplot( aes(x = OP_Score, fill = GPA_Round) ) + 
-  geom_bar(position = "fill") + 
-  labs( x = "OP Score ( 1 (highest) -> 25 (lowest) performance )", 
-        y = "proportion of students getting Each OP score", 
-        title = "OP Score Barchart (colored by Each GPA Group) ") 
-
-uniData %>%
-  mutate(GPA_Round = as.factor(round(GPA)) ) %>%
-  ggplot( aes(x = OP_Score) ) + 
-  geom_histogram(bins = 10, color="darkblue", fill="lightblue") + 
-  facet_wrap(~GPA_Round) +
-  labs( x = "OP Score ( 1 (highest) -> 25 (lowest) performance )", 
-        y = "Number of Students", 
-        title = "OP Score histogram (colored by Each GPA Group) ") 
+visualize_relationship_op_and_gpa(uniData)
 
 # Task 4 ----------------------------------------------------------
+# Linear Regression
+
+# Visualize Scatterplot by each numerical vs GPA
+# and calculate the correlation
+visualize_scatterplots_Vs_GPA(uniData)
+
+set.seed(2)
+library(caTools)
+
+split <- sample.split()
+
