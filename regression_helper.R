d84c362470eb7cfd7416b7eb89e2fbb598d5a916
Eric Lee <Raidenlkl@gmail.com>
2019-09-29 14:09:36 +1000

    Final Submition

--- regression_helper.R
+++ regression_helper.R
@@ -12,26 +12,28 @@ print_R2_and_pvalue <- function(null_deviance, deviance) {
 }
 
 print_MCE_Sens_Spec <- function(actuals,probability){
+  # Calculate Cut off
   optCutOff = optimalCutoff(actuals, probability)[1]
-  
+  # Print Cut off
   print(paste ("Optimal Cut off :", optCutOff))
-  
+  # Print MisClassification Error
   print(paste("MisClassification Error : ",  misClassError(actuals, probability, threshold = optCutOff ) ))
-  
+  # Print Sensitivity
   print(paste("sensitivity: ",  sensitivity(actuals, probability, threshold = optCutOff ) ))
-  
+  # Print Specificity
   print(paste("specificity : ", specificity( actuals, probability,threshold = optCutOff ) ))
 }
 
 print_ConfusionMatrix <- function(actuals, probability){
+  # Calculate Cut off
   optCutOff = optimalCutoff(actuals, probability)[1]
-  
+  # Print Confusion Matrix
   print( confusionMatrix( actuals, probability, threshold = optCutOff ))
 }
 
 
 visualize_ROC_Curve <- function(actuals, probability) {
+  # Display ROC Curve On Screen
   library(InformationValue)
-  
   plotROC(actuals = actuals, predictedScores = probability)
 }
